31d1e092a9a16a0a7488d6906a31f4d8577905dd
Prashant Shahi <PrashantShahi567@gmail.com>
2019-06-08 09:29:41 +0530

    Mutation - Adding referal data

--- app.py
+++ app.py
@@ -20,6 +20,69 @@ def main():
     op = pydgraph.Operation(schema=schema)
     client.alter(op)
 
+    # Mutate
+    myobj = {
+        'name': 'Arya',
+        'email': 'arya@got.com',
+        'age': 18,
+        'referred': [
+            {
+                'name': 'Bran',
+                'email': 'bran@got.com',
+                'age': 21
+            },
+            {
+                'name': 'Catelyn',
+                'email': 'catelyn@got.com',
+                'age': 39,
+                'referred': [
+                    {
+                       'name': 'Ned',
+                        'email': 'ned@got.com',
+                        'age': 41
+                    }
+                ]
+            },
+            {
+                'name': 'Jon',
+                'email': 'jon@got.com',
+                'age': 26,
+                'referred': [
+                    {
+                       'name': 'Daenerys',
+                        'email': 'daenerys@got.com',
+                        'age': 28,
+                        'referred': [
+                            {
+                               'name': 'Jorah',
+                                'email': 'jorah@got.com',
+                                'age': 30
+                            },
+                            {
+                               'name': 'Khal',
+                                'email': 'khal@got.com',
+                                'age': 32
+                            }
+                        ]
+                    },
+                    {
+                       'name': 'Ygritte',
+                        'email': 'ygritte@got.com',
+                        'age': 24
+                    }
+                ]
+            }
+        ]
+    }
+    txn = client.txn()
+    try:
+        assigned = txn.mutate(set_obj=myobj)
+        txn.commit()
+
+        print(assigned.uids)
+    finally:
+        txn.discard()
+
     # Close the client stub.
     client_stub.close()
 
