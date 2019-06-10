6d9622a03c6db53021c8bb54200969906d65a4b8
Prashant Shahi <PrashantShahi567@gmail.com>
2019-06-10 23:57:47 +0530

    Added new functions for querying and mutatation
    - Updated the GraphQL schema file
    - Added create customer and delete node funcitons

--- graphql-server/server.js
+++ graphql-server/server.js
@@ -11,22 +11,36 @@ var schema = buildSchema(schema_data.toString());
 // Creating function to fetch data
 // Traversal begins from salesman with the provided email identifier
 var getSalesman = (args) =>{
-    const email = args.email;
-    const person = axios.post('http://localhost:5000/salesman',{'email':email})
-        .then((response) => {                    
-            const data = response.data.data.all[0];
-            return data;
+    const payload = {
+        email: args.email,
+    };
+    const response_obj = axios.post('http://localhost:5000/salesman', payload)
+        .then((response) => {
+            const data = response.data;
+            if(data['status']==="success") {
+                console.log(JSON.stringify(data))
+                return data.data.all[0];
+            } else if(data['status']==="warning") {
+                console.log(data)
+                return data.data
+            } else if(data['status']==="error") {
+                console.log("Error: ", data['error'])
+                throw data['error']
+            } else {
+                throw "Unexpected error occurred"
+            }
         })
         .catch((error) => {
             // handle error
-            console.log(error);
+            console.log("ERROR: ", error);
+            throw error
         })
-    return person;
+        return response_obj;
 }
 
 // Creating function to add new Salesman
 var addSalesMan = (args) => {
-    payload = {
+    const payload = {
         name: args.name,
         age: args.age,
         email: args.email,
@@ -38,58 +52,121 @@ var addSalesMan = (args) => {
             if(data['status']==="success") {
                 console.log(data)
                 return data.data;
+            } else if(data['status']==="warning") {
+                console.log(data)
+                throw data["message"]
+                return data.data
             } else {
                 console.log("Error: ", data['error'])
-                return data
+                throw data['error']
             }
         })
         .catch((error) => {
             // handle error
             console.log("ERROR: ", error);
-            return  {
-                status: "error",
-                error: error
-            };
+            throw error
         })
         return response_obj;
 };
 
 // Creating function to add new Sales
 var addSales = (args) => {
-    payload = {
+    const payload = {
         item: args.item,
+        category: args.category,
+        invoice_no: args.invoice_no,
         store: args.store,
+        location: args.location,
         price: args.price,
         quantity: args.quantity,
-        salesman_email: args.salesman_email
+        total_amount: args.total_amount,
+        salesman_email: args.salesman_email,
+        customer_email: args.customer_email
     }
     const response_obj = axios.post('http://localhost:5000/create-sales', payload)
         .then((response) => {
-            console.log("response: ", response)
-            var data = response.data;
-            if(data.status=="success") {
-                console.log("data: ", data)
+            const data = response.data;
+            if(data['status']==="success") {
+                console.log(data)
                 return data.data;
+            } else if(data['status']==="warning") {
+                console.log(data)
+                throw data["message"]
+                return data.data
             } else {
-                console.log("Error: ", data.error)
-                return data
+                console.log("Error: ", data['error'])
+                throw data['error']
             }
         })
         .catch((error) => {
             // handle error
             console.log("ERROR: ", error);
-            return {
-                status: "error",
-                error: error
-            };
+            throw error
         })
         return response_obj;
 }
 
+// Creating function to add new Customer
+var addCustomer = (args) => {
+    const payload = {
+        name: args.name,
+        age: args.age,
+        email: args.email
+    };
+    const response_obj = axios.post('http://localhost:5000/create-customer', payload)
+        .then((response) => {
+            const data = response.data;
+            if(data['status']==="success") {
+                console.log(data)
+                return data.data;
+            } else if(data['status']==="warning") {
+                console.log(data)
+                throw data["message"]
+                return data.data
+            } else {
+                console.log("Error: ", data['error'])
+                throw data['error']
+            }
+        })
+        .catch((error) => {
+            // handle error
+            console.log("ERROR: ", error);
+            throw error
+        })
+        return response_obj;
+};
+
+// Creating function to add new Salesman
+var deleteNode = (args) => {
+    const payload = {
+        reference: args.reference,
+        value: args.value
+    };
+    const response_obj = axios.post('http://localhost:5000/delete-node', payload)
+        .then((response) => {
+            const data = response.data;
+            if(data['status']==="success") {
+                console.log(data)
+                return data;
+            } else {
+                console.log("Error: ", data['error'])
+                return data;
+            }
+        })
+        .catch((error) => {
+            // handle error
+            console.log("ERROR: ", error);
+            throw error
+        })
+        return response_obj;
+};
+
 var root = {
     addsalesman: addSalesMan,
+    addcustomer: addCustomer,
     addsales: addSales,
-    salesman: getSalesman,
+    deletenode: deleteNode,
+    salesman: getSalesman
 };
 
 // Create an express server and a GraphQL endpoint
